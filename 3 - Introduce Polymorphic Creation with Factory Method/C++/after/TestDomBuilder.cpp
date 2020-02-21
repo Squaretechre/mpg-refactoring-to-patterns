a0fd057a54e1174eb39825c8cfc4132cf936cba9
Jason Thorogood <jason.thorogood@fake.com>
2020-02-21 15:45:16 +0000

    Step 1: Extract common method

--- 3 - Introduce Polymorphic Creation with Factory Method/C++/after/TestDomBuilder.cpp
+++ 3 - Introduce Polymorphic Creation with Factory Method/C++/after/TestDomBuilder.cpp
@@ -4,9 +4,14 @@
 
 #include "DomBuilder.h"
 
+std::unique_ptr<IOutputBuilder> TestDomBuilder::CreateBuilder(std::string root)
+{
+  return std::unique_ptr<IOutputBuilder>(new DomBuilder(root));
+}
+
 void TestDomBuilder::TestAddAboveRoot()
 {
-  Builder = std::unique_ptr<DomBuilder>(new DomBuilder("orders"));
+  Builder = CreateBuilder("orders");
   ASSERT_NO_THROW(Builder->AddBelow("customer"));
   ASSERT_ANY_THROW(Builder->AddAbove("customer"));
 }
--- 3 - Introduce Polymorphic Creation with Factory Method/C++/after/TestDomBuilder.h
+++ 3 - Introduce Polymorphic Creation with Factory Method/C++/after/TestDomBuilder.h
@@ -3,14 +3,15 @@
 
 #include <gtest/gtest.h>
 
-#include "DomBuilder.h"
+#include "IOutputBuilder.h"
 
 class TestDomBuilder : public testing::Test
 {
 public:
   void TestAddAboveRoot();
 private:
-  std::unique_ptr<DomBuilder> Builder;
+  std::unique_ptr<IOutputBuilder> Builder;
+  std::unique_ptr<IOutputBuilder> CreateBuilder(std::string root);
 };
 
 #endif
