0f98b955a106fcd757514308da1249cefd3253ab
Andrew Pattison <andygjp@hotmail.com>
2020-03-10 21:33:57 +0000

    feat: Added the JS classes from session 4. Yet to refactor them to match the example

---
+++ 5 - Form Template Method/JS/Loan.test.js
@@ -0,0 +1,37 @@
+import Loan from "./Loan"
+
+describe('Capital calculation', () => {
+  const loanAmount = 10000
+  const highRiskRating = 1
+
+  it('capital term loan', () => {
+    const start = new Date(2003, 11, 20)
+    const maturity = new Date(2006, 11, 20)
+    const termLoan = Loan.NewTermLoan(loanAmount, start, maturity, highRiskRating)
+
+    termLoan.payment(1000, new Date(2004, 11, 20))
+    termLoan.payment(1000, new Date(2005, 11, 20))
+    termLoan.payment(1000, new Date(2006, 11, 20))
+
+    expect(termLoan.duration()).toBe(2)
+    expect(termLoan.capital()).toBe(210)
+  })
+
+  it('capital advised line', () => {
+    const start = new Date(2003, 11, 20)
+    const expiry = new Date(2005, 11, 20)
+    const loan = Loan.NewAdvisedLine(loanAmount, start, expiry, highRiskRating)
+
+    expect(loan.duration()).toBe(2)
+    expect(loan.capital()).toBe(21)
+  })
+
+  it('capital revolver', () => {
+    const start = new Date(2003, 11, 20)
+    const expiry = new Date(2006, 11, 20)
+    const loan = Loan.NewRevolver(loanAmount, start, expiry, highRiskRating)
+
+    expect(loan.duration()).toBe(3)
+    expect(loan.capital()).toBe(315)
+  })
+})
---
+++ 5 - Form Template Method/JS/UnusedRiskFactors.js
@@ -0,0 +1,5 @@
+export default class UnusedRiskFactors {
+  static ForRating (riskRating) {
+    return 0.0105
+  }
+}
