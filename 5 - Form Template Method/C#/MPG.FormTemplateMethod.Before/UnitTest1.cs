e2a4ea0c26079226e52d4f71728940c900fc711b
Andrew Pattison <andygjp@hotmail.com>
2020-03-10 13:52:24 +0000

    created the csharp projects

---
+++ 5 - Form Template Method/C#/MPG.FormTemplateMethod.Before/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MPG.FormTemplateMethod.Before
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
