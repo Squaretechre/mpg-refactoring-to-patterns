e9e589d6afb3b886f599b97044eb63a24b5a9cee
Andrew Pattison <andygjp@hotmail.com>
2020-03-11 10:32:26 +0000

    refactor: added an instructive comment to keep track of what I need to do

--- 5 - Form Template Method/C#/MPG.FormTemplateMethod.After/CapitalStrategyAdvisedLine.cs
+++ 5 - Form Template Method/C#/MPG.FormTemplateMethod.After/CapitalStrategyAdvisedLine.cs
@@ -4,6 +4,7 @@
     {
         public override double Capital(Loan loan)
         {
+            // TODO - x * duration * riskFactor is common in CapitalStrategyAdvisedLine & CapitalStrategyTermLoan
             return loan.GetCommitment() * loan.GetUnusedPercentage() * Duration(loan) * RiskFactor(loan);
         }
     }
--- 5 - Form Template Method/C#/MPG.FormTemplateMethod.After/CapitalStrategyTermLoan.cs
+++ 5 - Form Template Method/C#/MPG.FormTemplateMethod.After/CapitalStrategyTermLoan.cs
@@ -7,6 +7,7 @@
     {
         public override double Capital(Loan loan)
         {
+            // TODO - x * duration * riskFactor is common in CapitalStrategyAdvisedLine & CapitalStrategyTermLoan
             return loan.GetCommitment() * Duration(loan) * RiskFactor(loan);
         }
 
